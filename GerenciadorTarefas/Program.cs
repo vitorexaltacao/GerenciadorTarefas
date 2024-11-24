85b2d64d389f3c37ee55455d8a3e3054a24d0bbc
Bowie <jvexs17@gmail.com>
2024-11-24 05:56:09 -0300

    Integra TaskService no arquivo Program.cs

--- GerenciadorTarefas/Program.cs
+++ GerenciadorTarefas/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using GerenciadorTarefas.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var taskService = new TaskService();
+
+        taskService.AddTask("Estudar C#", "Estudar a estrutura de um projeto em C#");
+        taskService.AddTask("Praticar Git", "Fazer commits organizados");
+
+        var tasks = taskService.GetTasks();
+
+        Console.WriteLine("Tarefas:");
+        foreach (var task in tasks)
+        {
+            Console.WriteLine($"{task.Id}: {task.Name} - {(task.IsCompleted ? "Concluída" : "Pendente")}");
+        }
+    }
+}
+
